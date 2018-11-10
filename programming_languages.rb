7a1b16fbba47e6a18dcc8eab779a4ff968e0943e
Nathan Bell <nathanbell09@gmail.com>
2018-11-10 15:37:11 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -17,19 +17,19 @@ def reformat_languages(languages)
   binding.pry
 end
 
-# test_hash = {
-#   :oo => {
-#     :ruby => {:type => "interpreted"},
-#     :javascript => {:type => "interpreted"},
-#     :python => {:type => "interpreted"},
-#     :java => {:type => "compiled"}
-#   },
-#   :functional => {
-#     :clojure => {:type => "compiled"},
-#     :erlang => {:type => "compiled"},
-#     :scala => {:type => "compiled"},
-#     :javascript => {:type => "interpreted"}
-#   }
-# }
-#
-# reformat_languages(test_hash)
+test_hash = {
+  :oo => {
+    :ruby => {:type => "interpreted"},
+    :javascript => {:type => "interpreted"},
+    :python => {:type => "interpreted"},
+    :java => {:type => "compiled"}
+  },
+  :functional => {
+    :clojure => {:type => "compiled"},
+    :erlang => {:type => "compiled"},
+    :scala => {:type => "compiled"},
+    :javascript => {:type => "interpreted"}
+  }
+}
+
+reformat_languages(test_hash)
