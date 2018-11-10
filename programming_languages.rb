2d08be6ab5dcc25275ec1aa9f6f6e83622733916
Nathan Bell <nathanbell09@gmail.com>
2018-11-10 16:25:57 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -7,12 +7,15 @@ def reformat_languages(languages)
   languages.each do |prog_style, langs|
     langs.each do |lang_name, lang_type|
       new_hash[lang_name] = lang_type
-      testing = languages[:oo].include?(:javascript) && languages[:functional].include?(:javascript)
-      binding.pry
+      if new_hash.include?(lang_name) && langs.include?(lang_name)
+        new_hash[lang_name][:style] << prog_style
+      else
+        new_hash[lang_name][:style] = [prog_style]
+      end
     end
   end
   new_hash
-
+  binding.pry
 end
 
 test_hash = {
