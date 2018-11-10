f2a546c2da9ae138125679e1a9a1155a67096b40
Nathan Bell <nathanbell09@gmail.com>
2018-11-10 15:23:05 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,11 @@
+require "pry"
+
 def reformat_languages(languages)
   # your code here
+  new_hash = {}
+  languages.each do |style, langs|
+    langs.each do |name, type|
+      
+    end
+  end
 end
