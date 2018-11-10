95200e160364274ca9aba56f5e197050f25b1dda
Nathan Bell <nathanbell09@gmail.com>
2018-11-10 16:59:54 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -4,12 +4,10 @@ def reformat_languages(languages)
   # your code here
   new_hash = {}
   style_array = []
-  included_lang = []
   languages.each do |prog_style, langs|
     langs.each do |lang_name, lang_type|
       new_hash[lang_name] = lang_type
-      included_lang << lang_name
-      if included_lang.include?(lang_name) && langs.include?(lang_name)
+      if false
         new_hash[lang_name][:style] << prog_style
       else
         style_array = [prog_style]
